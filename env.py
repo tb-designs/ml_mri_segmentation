9ad725a0a9ec2afb52484c15256114a4daf3f0db
tb_designs <ryanblais@uvic.ca>
2021-06-14 17:47:18 -0700

    added dataset access and test image visualization

--- env.py
+++ env.py
@@ -1,5 +1,4 @@
 import os
-import cv2
 import glob
 import PIL
 import shutil
@@ -7,11 +6,11 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scikit-image import data
-from scikit-image.util import montage 
-import scikit-image.transform as skTrans
-from scikit-image.transform import rotate
-from scikit-image.transform import resize
+from skimage import data
+from skimage.util import montage 
+import skimage.transform as skTrans
+from skimage.transform import rotate
+from skimage.transform import resize
 from PIL import Image, ImageOps  
 
 
@@ -19,7 +18,6 @@ from PIL import Image, ImageOps
 import nilearn as nl
 import nibabel as nib
 import nilearn.plotting as nlplt
-!pip install git+https://github.com/miykael/gif_your_nifti # nifti to gif 
 import gif_your_nifti.core as gif2nif
 
 
@@ -29,9 +27,9 @@ import keras.backend as K
 from keras.callbacks import CSVLogger
 import tensorflow as tf
 from tensorflow.keras.utils import plot_model
-from scikit-learn.preprocessing import MinMaxScaler
-from scikit-learn.model_selection import train_test_split
-from scikit-learn.metrics import classification_report
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
@@ -41,3 +39,78 @@ from tensorflow.keras.layers.experimental import preprocessing
 
 # Make numpy printouts easier to read.
 np.set_printoptions(precision=3, suppress=True)
+
+# DEFINE seg-areas  
+SEGMENT_CLASSES = {
+    0 : 'NOT tumor',
+    1 : 'NECROTIC/CORE', # or NON-ENHANCING tumor CORE
+    2 : 'EDEMA',
+    3 : 'ENHANCING' # original 4 -> converted into 3 later
+}
+
+# there are 155 slices per volume
+# to start at 5 and use 145 slices means we will skip the first 5 and last 5 
+VOLUME_SLICES = 100 
+VOLUME_START_AT = 22 # first slice of volume that we will include
+
+# Test to see images
+TRAIN_DATASET_PATH = '../BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/'
+VALIDATION_DATASET_PATH = '../BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData'
+
+test_image_flair=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_flair.nii').get_fdata()
+test_image_t1=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_t1.nii').get_fdata()
+test_image_t1ce=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_t1ce.nii').get_fdata()
+test_image_t2=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_t2.nii').get_fdata()
+test_mask=nib.load(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_seg.nii').get_fdata()
+
+
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1,5, figsize = (20, 10))
+slice_w = 25
+ax1.imshow(test_image_flair[:,:,test_image_flair.shape[0]//2-slice_w], cmap = 'gray')
+ax1.set_title('Image flair')
+ax2.imshow(test_image_t1[:,:,test_image_t1.shape[0]//2-slice_w], cmap = 'gray')
+ax2.set_title('Image t1')
+ax3.imshow(test_image_t1ce[:,:,test_image_t1ce.shape[0]//2-slice_w], cmap = 'gray')
+ax3.set_title('Image t1ce')
+ax4.imshow(test_image_t2[:,:,test_image_t2.shape[0]//2-slice_w], cmap = 'gray')
+ax4.set_title('Image t2')
+ax5.imshow(test_mask[:,:,test_mask.shape[0]//2-slice_w])
+ax5.set_title('Mask')
+
+# Skip 50:-50 slices since there is not much to see
+fig1, ax1 = plt.subplots(1, 1, figsize = (15,15))
+ax1.imshow(rotate(montage(test_image_t1[50:-50,:,:]), 90, resize=True), cmap ='gray')
+
+# Show the tumor segments over each slice
+fig, ax1 = plt.subplots(1, 1, figsize = (15,15))
+ax1.imshow(rotate(montage(test_mask[60:-60,:,:]), 90, resize=True), cmap ='gray')
+
+#gif time
+shutil.copy2(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_flair.nii', './test_gif_BraTS20_Training_001_flair.nii')
+gif2nif.write_gif_normal('./test_gif_BraTS20_Training_001_flair.nii')
+
+# Add effects to show segments
+niimg = nl.image.load_img(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_flair.nii')
+nimask = nl.image.load_img(TRAIN_DATASET_PATH + 'BraTS20_Training_001/BraTS20_Training_001_seg.nii')
+
+fig, axes = plt.subplots(nrows=4, figsize=(30, 40))
+
+
+nlplt.plot_anat(niimg,
+                title='BraTS20_Training_001_flair.nii plot_anat',
+                axes=axes[0])
+
+nlplt.plot_epi(niimg,
+               title='BraTS20_Training_001_flair.nii plot_epi',
+               axes=axes[1])
+
+nlplt.plot_img(niimg,
+               title='BraTS20_Training_001_flair.nii plot_img',
+               axes=axes[2])
+
+nlplt.plot_roi(nimask, 
+               title='BraTS20_Training_001_flair.nii with mask plot_roi',
+               bg_img=niimg, 
+               axes=axes[3], cmap='Paired')
+
+plt.show()
